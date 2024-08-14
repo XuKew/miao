721b63c992a2220b2e8f0846ffeb6400cca130aa
KW <xukewei53@gmail.com>
2024-08-14 17:20:34 +0800

    lodash

--- lodash/xukew-lodash.js
+++ lodash/xukew-lodash.js
@@ -51,8 +51,67 @@ var xukew = function () {
         }
         return -1
     }
+    function flatten(array) {
+        var flattenArray = []
+        for (var i = 0; i < array.length; i++) {
+            var result = array[i]
+            if (Array.isArray(result)) {
+                for (let j = 0; j < result.length; j++) {
+                    flattenArray.push(result[i])
+                }
+            } else {
+                flattenArray.push(result)
+            }
+        }
+        return flattenArray
+    }
+    function flattenDeep(array) {
+        var flattenArray = []
+        for (var i = 0; i < array.length; i++) {
+            var result = array[i]
+            if (Array.isArray(result)) {
+                for (var res of flattenDeep(result)) {
+                    flattenArray.push(res)
+                }
+            } else {
+                flattenArray.push(result)
+            }
+        }
+        return flattenArray
+    }
+    function fromPairs(pairs) {
+        return pairs.reduce((obj, [key, value]) => {
+            obj[key] = value
+            return obj
+        }, {})
+    }
+    function head(array) {
+        console.log(array[0])
+    }
+    function indexOf(array, value, fromIndex = 0) {
+        if (array == null) {
+            return -1
+        }
+        for (i = fromIndex; i < array.length; i++) {
+            if (array[i] == value) {
+                return i
+            }
+        }
+    }
+    function initial(array) {
+        var Newarray = []
+        for (var i = 0; i < array.length - 1; i++) {
+            Newarray.push(array[i])
+        }
+        return Newarray
+    }
 
-
+    function intersection(array1, array2) {
+        var set1 = new Set(array1)
+        var set2 = new Set(array2)
+        var intersectionSet = new Set(array1.filter(value => set2.has(value)))
+        return Array.from(intersectionSet)
+    }
 
     return {
         chunk: chunk,
@@ -60,6 +119,13 @@ var xukew = function () {
         difference: difference,
         fill: fill,
         drop: drop,
+        flatten: flatten,
+        flattenDeep: flattenDeep,
+        fromPairs: fromPairs,
+        head: head,
+        indexOf: indexOf,
+        initial: initial,
+        intersection: intersection
 
     }
 }()
